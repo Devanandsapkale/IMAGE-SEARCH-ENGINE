deb8bc2f71a0bbf354969564cd99f6009bae0dab
Devanand Sapkale <kolideva54@gmail.com>
2024-03-11 20:36:09 +0530

    Image-Search-Engine Project

---
+++ imgserch.js
@@ -0,0 +1,4 @@
+const searchForm= document.getElementById("search-form");
+const searchBox= document.getElementById("search-bo");
+const searchResult= document.getElementById("search-result");
+const showMoreBtn= document.getElementById("show-more-btn");
